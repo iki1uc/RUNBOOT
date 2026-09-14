844d8599772afdf51ae73077e9fe9a382630ee41
iki1uc <entertrainer@web.de>
2026-09-14 17:56:07 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,13 +1,27 @@
-import {
-  DOOR,
-  DOO,
-  RESPO,
-  OS,
-  NC_engine,
-  _243,
-  SCORE,
-  WETTE,
-  SLIDE
-} from "./boot.js";
+// SYN + QUANDT Wächter
+function energyWatch() {
 
-boot();
+    // Energie prüfen
+    if (REAL_INPUT.quandt.cap <= 20) {
+        console.log("QUANDT LOW → safe shutdown");
+        SCORE.write("shutdown");
+        AXIOM.jump(OS.ENTRY);
+        return false;
+    }
+
+    // SYN prüfen
+    if (!SYN.alive) {
+        console.log("SYN LOST → safe shutdown");
+        SCORE.write("shutdown");
+        AXIOM.jump(OS.ENTRY);
+        return false;
+    }
+
+    // Reset für nächsten Zyklus
+    SYN.alive = false;
+
+    // Roundwork val++
+    BOOT_GEO.val++;
+
+    return true;
+}
