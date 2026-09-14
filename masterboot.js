2fc8ac47c369a10ffbd0be636631dfabc969ed4b
iki1uc <entertrainer@web.de>
2026-09-14 17:57:45 +0200

    Update masterboot.js

--- masterboot.js
+++ masterboot.js
@@ -1,25 +1,36 @@
-masterboot = {
+// masterboot.js · iki1uc · repariert
 
-  load: async function(file) {
-    return await OS.net.read(file)
+import { BOOT_GEO } from "./boot-geo.js";
+
+const masterboot = {
+
+  load: async function() {
+    // BOOT.geo kommt lokal aus boot-geo.js
+    return BOOT_GEO;
   },
 
   apply: function(geo) {
-    OS.zone = geo.zone
-    OS.pos  = geo.pos
-    OS.dir  = geo.dir
-    OS.val  = geo.val
-    OS.mov  = geo.mov
-    OS.memory = geo.memory
+    OS.zone   = geo.zone;
+    OS.pos    = geo.pos;
+    OS.dir    = geo.dir;
+    OS.val    = geo.val;
+    OS.mov    = geo.mov;
+    OS.memory = geo.memory || {};
   },
 
   start: async function() {
 
-    if (!OS.net.online()) {
-      throw "NET required: System cannot start offline."
-    }
+    // alte Netzprüfung entfernt → freeboote läuft offline
+    let geo = await this.load();
 
-    let geo = await this.load("BOOT.geo")
-    this.apply(geo)
+    // Geo-Werte ins OS übernehmen
+    this.apply(geo);
+
+    // Roundwork initialisieren
+    SCORE.write("masterboot.start");
+    _243.sync();     // Takt
+    SYN.tick();      // SYN lebt
   }
-}
+};
+
+export { masterboot };
